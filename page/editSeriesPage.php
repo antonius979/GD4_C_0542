78dc1560d3431b1b5646df7eeeca7de1e2268fd6
antonius979 <antoniusindra979@gmail.com>
2022-09-25 01:45:04 +0700

    update ugd tugas

--- page/editSeriesPage.php
+++ page/editSeriesPage.php
@@ -8,10 +8,10 @@
     $genre = array('Romance', 'Action', 'Comedy');
 ?>
 
-< <div class="container p-3 m-4 h-100" style="background-color: #FFFFFF; border-top: 5px 
+<div class="container p-3 m-4 h-100" style="background-color: #FFFFFF; border-top: 5px 
  solid #D40013; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 
  0.19);" >
-    <h4 >EDIT DATA MOVIES</h4>
+    <h4 >EDIT DATA SERIES</h4>
     <a href="./editSeriesPage.php"></a>
     <hr>
     <form action="../process/editSerieProcess.php" method="post">
@@ -49,7 +49,7 @@
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Synopsis</label>
-            <textarea class="form-control" id="synopsis" name="synopsis" aria-describedby="emailHelp"value="<?php echo $data['synopsis']; ?>"></textarea>
+            <input class="form-control" id="synopsis" name="synopsis" aria-describedby="emailHelp"value="<?php echo $data['synopsis']; ?>"></input>
         </div>
         <div class="d-grid gap-2">
             <button type="submit" class="btn btn-primary " name="edit" style="background: black" >Save</button>
